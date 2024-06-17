ed1d1dd6a0d8fbffa05ea6994f0a5de9031bf090
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-17 23:37:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let student={
+	name:"sam"
+}
+
+Object.prototype.getKeys=function(){
+	let arr=[];
+	let obj=this;
+	for(let t in obj) arr.push(t);
+	return arr;
+}
